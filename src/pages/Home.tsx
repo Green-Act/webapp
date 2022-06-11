3ca4dba1f28fd11c3955047f0024a7c85bac388e
Shadab Khan <shadab.2005@gmail.com>
2022-06-11 23:15:14 +0530

    feat: wip home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home: React.FC<Record<string, never>> = () => {
-  return null;
+  return (
+    <div className="h-[calc(100%-64px)] w-full">
+      <div className="flex items-center justify-center space-x-16 py-40 px-20 h-full">
+        <div className="rounded-xl bg-gat-gray h-full w-1/3"></div>
+        <div className="rounded-xl bg-gat-gray h-full w-1/3"></div>
+        <div className="rounded-xl bg-gat-gray h-full w-1/3"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
