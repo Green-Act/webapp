3703fb8c013cfccd468fd94b3ad6a3c0cb1a1b36
Shadab Khan <shadab.2005@gmail.com>
2022-06-14 00:04:29 +0530

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Header />
-      <main className="h-[calc(100%-64px)] w-full bg-gat bg-blend-normal bg-cover bg-center bg-no-repeat">
+      <main className="min-h-[calc(100%-64px)] w-full bg-gat bg-blend-normal bg-cover bg-center bg-no-repeat relative">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </main>
     </Router>
   );
