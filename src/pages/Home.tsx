b0fc2bb0d0557e75dc2f964d414f041850553689
Shadab Khan <shadab.2005@gmail.com>
2022-06-13 18:34:53 +0530

    feat: new header design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Home: React.FC<Record<string, never>> = () => {
   return (
-    <div className="h-[calc(100%-64px)] w-full">
-      <div className="flex items-center justify-center space-x-16 py-40 px-20 h-full">
+    <div className="h-full w-full px-12">
+      <div className="flex items-center justify-center space-x-16 py-40 h-full">
         <div className="rounded-xl bg-gat-gray h-full w-1/3"></div>
         <div className="rounded-xl bg-gat-gray h-full w-1/3"></div>
         <div className="rounded-xl bg-gat-gray h-full w-1/3"></div>
