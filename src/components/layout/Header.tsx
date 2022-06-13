6f55da0dca6d8655e012ff5ee7ba9892c7c5b928
Shadab Khan <shadab.2005@gmail.com>
2022-06-13 18:34:53 +0530

    feat: new header design

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,25 +3,25 @@ import { NavLink } from "react-router-dom";
 
 const Header: React.FC<Record<string, never>> = () => {
   return (
-    <header className="flex items-center justify-between h-16 px-8 py-4">
-      <img src="/logo.svg" className="h-full" />
+    <header className="flex items-center justify-between h-16 px-12 py-4 shadow-sm shadow-gat-green/10 relative z-1 text-xs">
+      <img src="/logo.svg" className="h-8" />
       <nav className="flex-1 flex items-center justify-center">
         <NavLink
           to="/"
           className={({ isActive }) =>
             `${
-              isActive ? "bg-gat-gray" : ""
-            } rounded-md py-2 w-24 text-center text-sm font-bold`
+              isActive ? "bg-gat-green/10" : ""
+            } rounded-full py-2 w-28 text-center font-bold`
           }
         >
-          Stats
+          Home
         </NavLink>
         <NavLink
           to="/challenges"
           className={({ isActive }) =>
             `${
-              isActive ? "bg-gat-gray" : ""
-            } rounded-md py-2 w-24 text-center text-sm font-bold mx-2`
+              isActive ? "bg-gat-green/10" : ""
+            } rounded-full py-2 w-28 text-center font-bold mx-2`
           }
         >
           Challenges
@@ -30,14 +30,14 @@ const Header: React.FC<Record<string, never>> = () => {
           to="/marketplace"
           className={({ isActive }) =>
             `${
-              isActive ? "bg-gat-gray" : ""
-            } rounded-md py-2 w-24 text-center text-sm font-bold`
+              isActive ? "bg-gat-green/10" : ""
+            } rounded-full py-2 w-28 text-center font-bold`
           }
         >
           Marketplace
         </NavLink>
       </nav>
-      <button className="bg-gat-green px-8 py-2 rounded-full font-bold">
+      <button className="border border-gat-green px-12 py-2 rounded-full font-bold">
         Connect Wallet
       </button>
     </header>
