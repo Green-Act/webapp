a467303b07cf6daafd1362b43aa689ab1b612f59
Kevaudib <audibertikev@gmail.com>
2022-06-20 21:01:33 +0200

    feat: add scripts to reward customers
    Function to send reward to customer

---
+++ scripts/interact.js
@@ -0,0 +1,30 @@
+//interact.js
+require("dotenv").config();
+const { ethers } = require("ethers");
+const API_KEY = process.env.API_KEY;
+const ***REMOVED*** = process.env.***REMOVED***;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const contract = require("./greenhacktoken.json");
+console.log(JSON.stringify(contract));
+
+// Provider
+const provider = new ethers.providers.InfuraProvider(
+  (network = "maticmum"),
+  API_KEY
+);
+// Signer
+const signer = new ethers.Wallet(***REMOVED***, provider);
+// Contract
+const greenHackTokenContract = new ethers.Contract(
+  CONTRACT_ADDRESS,
+  contract,
+  signer
+);
+
+async function reward(amount_matic, to) {
+  amount = ethers.utils.parseUnits(amount_matic, "18");
+  const tx = await greenHackTokenContract.mint(to, amount);
+  console.log(amount_matic + " GHK have been minted for " + to);
+}
+
+reward("5", "0xa4437699EA5A31E85546fcc634f46E164E2D1246");
