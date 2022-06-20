7c8a8e879d5c427733315ddb07ff0b7b3d30be41
Kevaudib <audibertikev@gmail.com>
2022-06-20 14:30:14 +0200

    fix: props types for Challenge cards
    detail: props types for Challenge cards

--- src/components/ChallengeCard.tsx
+++ src/components/ChallengeCard.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const ChallengeCard: React.FC<Record<string, never>> = (props) => {
+const ChallengeCard: React.FC<{
+  key: string;
+  category: string;
+  level: string;
+  description: string;
+  reward: string;
+}> = (props) => {
   return (
     <div className="flex flex-col items-center justify-between rounded-xl bg-white border border-gat-green shadow-md shadow-black/20 mx-8 my-4 h-[240px] w-[240px]">
       <div className="flex flex-row justify-between border-b-[1px] border-gat-green w-full px-4 py-2 text-base font-medium">
@@ -16,9 +22,11 @@ const ChallengeCard: React.FC<Record<string, never>> = (props) => {
         <div className="text-right ml-auto">{props.reward}</div>
         <img src="/leaf.svg" className="pl-2" />
       </div>
-      <button className="border-2 border-gat-green w-[100px] py-1 rounded-full font-bold text-xs mb-2 my-2">
-        {" "}
-        Submit!{" "}
+      <button
+        className="border-2 border-gat-green w-[100px] py-1 rounded-full font-bold text-xs mb-2 my-2"
+        //onClick={()=>transfer()}
+      >
+        Submit!
       </button>
     </div>
   );
