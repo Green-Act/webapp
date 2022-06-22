8c8cdfd0ff38fa754293835ac31c752906acd5a3
Kevaudib <audibertikev@gmail.com>
2022-06-22 10:12:14 +0200

    fix: change logo and alignment of tasks
    fix: change logo and alignment of tasks

--- src/pages/Challenges.tsx
+++ src/pages/Challenges.tsx
@@ -14,11 +14,11 @@ const challengeCards = ChallengeData.map((item) => (
 
 const Challenges: React.FC<Record<string, never>> = () => {
   return (
-    <div className="h-full w-full px-12">
-      <div className="text-3xl font-bold text-center py-8 w-full">
+    <div className="h-full w-full">
+      <div className="text-3xl font-bold text-center py-8">
         Time to Green<span className="text-gat-green">Act</span>!
       </div>
-      <div className="flex flex-row flex-wrap items-start ">
+      <div className="flex flex-row flex-wrap items-start justify-center">
         {challengeCards}
       </div>
     </div>
